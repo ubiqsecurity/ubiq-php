ab2ce83af72a4c2bff77c0be52b729764b78c571
Sam Craig <sam@transeosolutions.com>
2023-06-27 14:56:02 -1000

    Update multi curl Fix event reporting format

--- src/Decryption.php
+++ src/Decryption.php
@@ -73,16 +73,14 @@ class Decryption
         }
 
         $this->_creds->eventprocessor->addOrIncrement(
-            new Event(
-                [
+            new Event([
                 'api_key'                   => $this->_creds->getPapi(),
                 'dataset_name'              => $this->_dataset->name,
                 'dataset_group_name'        => $this->_dataset->group_name,
-                'billing_action'            => EventProcessor::EVENT_TYPE_DECRYPT,
+                'action'                    => EventProcessor::EVENT_TYPE_DECRYPT,
                 'dataset_type'              => $this->_dataset->type,
                 'key_number'                => 0,
-                ]
-            )
+            ])
         );
 
         $this->_iv = '';
